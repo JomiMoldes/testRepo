541b55fe279a4db1fc271f2152aeb73bb5a617df
JomiMoldes <josemiguel.moldes@gmail.com>
2017-09-01 11:44:02 -0300

    project initialization

---
+++ TestProjectGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProjectGit
+//
+//  Created by MIGUEL MOLDES on 1/9/17.
+//  Copyright © 2017 MIGUEL MOLDES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
